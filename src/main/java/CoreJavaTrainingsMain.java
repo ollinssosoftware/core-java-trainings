11e7cbde966bf6628afcda5ca780e0fe0f3b02e0
Ollinsso Software <ollinssosoftware@gmail.com>
2022-12-11 23:57:13 +0530

    Update 1
    d

--- src/main/java/CoreJavaTrainingsMain.java
+++ src/main/java/CoreJavaTrainingsMain.java
@@ -2,5 +2,6 @@ public class CoreJavaTrainingsMain {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Changes done");
+        System.out.println("Changes done1");
     }
 }
